b904c4a39c3dd12cc72b3658bd0d0fe6733c1ab1
sujithkupen <sujithkumarpendem@gmail.com>
2024-03-26 12:01:36 +0530

    Add db connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,8 +8,10 @@ const {errorHandler} = require('./middleware/errorHandler');
 const cookieParser = require("cookie-parser");
 const cors= require('cors');
 const corsOptions = require('./config/corsOptions');
-
-
+const connectDB = require('./config/dbConn');
+const mongoose = require("mongoose");
+const {logEvents} = require('./middleware/logger');
+connectDB();
 app.use(cors(corsOptions));
 app.use(logger);
 app.use(cookieParser());
@@ -29,6 +32,14 @@ app.all('*', (req, res) => {
 });
 app.use(errorHandler);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+mongoose.connection.once('open', () => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+    logEvents(`${err}`,'error.log');
 });
